8cf7dd503982565d68cf02ac06674fef94ff0e06
Federico Gratti <federicogratti@gmail.com>
2026-01-20 14:51:01 -0300

    Add Notification model

---
+++ app/models/notification.rb
@@ -0,0 +1,5 @@
+class Notification < ApplicationRecord
+  attr_accessor :title, :content
+  
+  validates :phone_number, presence: true, format: { with: /\A\+\d{10,15}\z/, message: "must be in E.164 format" }
+end
