81ac75c332b046a33bca52eb89e8c08cb04d5b13
Federico Gratti <federicogratti@gmail.com>
2026-01-20 14:53:03 -0300

    Added content validation

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,6 @@
 class Notification < ApplicationRecord
   attr_accessor :title, :content
-  
+
   validates :phone_number, presence: true, format: { with: /\A\+\d{10,15}\z/, message: "must be in E.164 format" }
+  validates :content, presence: true, length: { maximum: 160 }
 end
